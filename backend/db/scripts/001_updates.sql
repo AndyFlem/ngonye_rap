975cf080c49a65c416ca05c1b0e4bd3bdb85449e
Andy Fleming <andy@ulendo.com>
2026-05-19 12:05:40 +0200

    Update

--- backend/db/scripts/001_updates.sql
+++ backend/db/scripts/001_updates.sql
@@ -9,5 +9,12 @@ UPDATE person set nhs = pairs.nhs from
 (select nhs,pah from person where household_head=true and nhs is not null) as pairs
 where
 pairs.pah=person.pah;
-    
+
+UPDATE person set created_at=hs.survey_date::date
+FROM
+	households_survey hs
+where person.pah=hs.pah;
+
+update person set created_at='2022-05-01' where created_at is null;
+
 commit;
